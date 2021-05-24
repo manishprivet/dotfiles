bba32ed7737f2a69c0580c053d2b568ed4a96f1e
Manish Kumar <manishprivet@protonmail.com>
2021-05-24 10:46:08 +0530

    Added Gnome config

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,8 +1,9 @@
 echo "Deleting Old Configuration Files"
 rm -f ~/.zshrc
 rm -f ~/.gitconfig
+rm -rf ~/.themes
 
 echo "Creating Symlinks for existing config files"
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.themes ~/.themes
+ln -s ~/.dotfiles/.themes ~/
